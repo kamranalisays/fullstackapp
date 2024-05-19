990a4a2de4e93362ab760c5bfed4856a3d3f9cd9
KAMRAN\kamranali <kamranali.says@gmail.com>
2024-05-19 05:14:04 +0100

    Importing Routers in App.js file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # fullstackapp
+
+
+# Frontend
+npx create-react-app frontend
+
+npm install react-router-dom@6
---
+++ frontend/src/components/UserComponents/Users.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Users= () => {
+
+        return <h1> list of users </h1>;
+
+};
+
+
+export default Users;
